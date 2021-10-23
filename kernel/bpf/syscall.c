988068c32827e336324d0cd0f6d7a05369d5a04b
Pzqqt <821026875@qq.com>
2021-10-23 15:06:04 +0800

    bpf: Mark proc_dointvec_minmax_bpf_restricted as "maybe unused"

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -265,7 +265,7 @@ static int map_create(union bpf_attr *attr)
 
 	err = security_bpf_map_alloc(map);
 	if (err)
-		goto free_map;
+		goto free_map_nouncharge;
 
 	err = bpf_map_charge_memlock(map);
 	if (err)
@@ -983,7 +983,7 @@ static int bpf_prog_detach(const union bpf_attr *attr)
 
 SYSCALL_DEFINE3(bpf, int, cmd, union bpf_attr __user *, uattr, unsigned int, size)
 {
-	union bpf_attr attr = {};
+	union bpf_attr attr;
 	int err;
 
 	if (sysctl_unprivileged_bpf_disabled && !capable(CAP_SYS_ADMIN))
@@ -1019,6 +1019,7 @@ SYSCALL_DEFINE3(bpf, int, cmd, union bpf_attr __user *, uattr, unsigned int, siz
 	}
 
 	/* copy attributes from user space, may be less than sizeof(bpf_attr) */
+	memset(&attr, 0, sizeof(attr));
 	if (copy_from_user(&attr, uattr, size) != 0)
 		return -EFAULT;
 
