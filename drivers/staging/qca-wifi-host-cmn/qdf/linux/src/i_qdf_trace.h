34ff8d085cd6381103c0ba31d78f65bd7ecb19d4
RaghuVarma331 <raghuvarma331@gmail.com>
2023-03-15 19:38:27 +0100

    Revert "staging: qca-wifi-host-cmn: Featurize QDF_ASSERT"
    This reverts commit a8b2c63486d23359598628b7b57d4ae4ea82de7a.

--- drivers/staging/qca-wifi-host-cmn/qdf/linux/src/i_qdf_trace.h
+++ drivers/staging/qca-wifi-host-cmn/qdf/linux/src/i_qdf_trace.h
@@ -96,7 +96,6 @@ static inline void no_qdf_trace_hex_dump(QDF_MODULE_ID module, QDF_TRACE_LEVEL l
 
 #ifdef QDF_ENABLE_TRACING
 
-#ifdef WLAN_WARN_ON_ASSERT
 #define QDF_ASSERT(_condition) \
 	do { \
 		if (!(_condition)) { \
@@ -105,14 +104,6 @@ static inline void no_qdf_trace_hex_dump(QDF_MODULE_ID module, QDF_TRACE_LEVEL l
 			WARN_ON(1); \
 		} \
 	} while (0)
-#else
-#define QDF_ASSERT(_condition) \
-	do { \
-		if (!(_condition)) { \
-			/* no-op */ \
-		} \
-	} while (0)
-#endif /* WLAN_WARN_ON_ASSERT */
 
 #else
 
